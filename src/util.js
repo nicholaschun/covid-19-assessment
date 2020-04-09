3914ac7c3835d7409c381e7572cb3747703129a4
nicholaschun <nicholaschunrayne@gmail.com>
2020-04-09 17:24:33 +0000

    edited week

--- src/util.js
+++ src/util.js
@@ -2,7 +2,7 @@ const calCulateActualDay = (periodType, timeToElapse) => {
   let result = null;
   if (periodType.toLowerCase() === 'days') {
     result = parseInt(timeToElapse, 10);
-  } else if (periodType.toLowerCase() === 'weeks') {
+  } else if (periodType.toLowerCase() === 'weeks' || periodType.toLowerCase() === 'week') {
     result = Math.floor(parseInt(timeToElapse, 10) * 7);
   } else if (periodType.toLowerCase() === 'months') {
     result = Math.floor(parseInt(timeToElapse, 10) * 30);
