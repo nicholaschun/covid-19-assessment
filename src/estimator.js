504ba87c2b4a0b9e68686db6b7f55f93aa9bf505
nicholaschun <nicholaschunrayne@gmail.com>
2020-04-09 15:36:28 +0000

    Added challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,26 @@
-const covid19ImpactEstimator = (data) => data;
+import calCulateActualDay from './util';
+
+const infectionByTimeRequest = (currentlyInfected, periodType, days) => {
+  const actualDays = calCulateActualDay(periodType, days);
+  return Math.floor(currentlyInfected * 2 ** actualDays);
+};
+
+const covid19ImpactEstimator = (data) => {
+  const output = {
+    data,
+    impact: {},
+    severeImpact: {}
+  };
+
+  output.impact.currentlyInfected = data.reportedCases * 10;
+
+  output.severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  output.impact.infectionByTimeRequest = infectionByTimeRequest(
+    output.impact.currentlyInfected,
+    data.periodType,
+    data.timeToElapse
+  );
+};
 
 export default covid19ImpactEstimator;
---
+++ src/util.js
@@ -0,0 +1,15 @@
+const calCulateActualDay = (periodType, timeToElapse) => {
+  let result = null;
+  if (periodType === 'days') {
+    result = Math.floor(timeToElapse / 3);
+  } else if (periodType === 'weeks') {
+    result = Math.floor((timeToElapse * 7) / 3);
+  } else if (periodType === 'months') {
+    result = Math.floor((timeToElapse * 30) / 3);
+  } else {
+    return null;
+  }
+  return result;
+};
+
+module.exports = calCulateActualDay;
