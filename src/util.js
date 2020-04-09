ae998a43fa18411270e9072b2281c929da8080d0
nicholaschun <nicholaschunrayne@gmail.com>
2020-04-09 16:51:43 +0000

    convert days to lower

--- src/util.js
+++ src/util.js
@@ -1,10 +1,10 @@
 const calCulateActualDay = (periodType, timeToElapse) => {
   let result = null;
-  if (periodType === 'days') {
+  if (periodType.toLowerCase() === 'days') {
     result = timeToElapse;
-  } else if (periodType === 'weeks') {
+  } else if (periodType.toLowerCase() === 'weeks') {
     result = Math.floor(timeToElapse * 7);
-  } else if (periodType === 'months') {
+  } else if (periodType.toLowerCase() === 'months') {
     result = Math.floor(timeToElapse * 30);
   } else {
     return null;
