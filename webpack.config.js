df4f8c191da09159745abe7f6ba06d2e91cd7ebe
Nicholas <nicholaschunrayne@gmail.com>
2020-04-10 23:37:47 +0000

    Frontend Challenge
    * Added challenge one
    
    * edited output
    
    * changed output datastructure
    
    * change export statement
    
    * changed call actual day
    
    * corrected spellings
    
    * convert days to lower
    
    * added challenge two and three
    
    * edited week
    
    * completed challenge 4
    
    * changed floor to trunc
    
    * corrected decimal issue
    
    * updated dollars in flight
    
    * converted period type
    
    * added netlify deploy and server
    
    * removed server file
    
    * updated app properties file
    
    * updated workflow yml
    
    * changed color of title

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
