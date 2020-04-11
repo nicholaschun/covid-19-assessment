d218f58adf26d456e279a9904572bbab67abb311
nicholaschun <nicholaschunrayne@gmail.com>
2020-04-11 01:14:07 +0000

    added express and heroku

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 require('body-parser-xml')(bodyParser);
 
@@ -11,11 +14,17 @@ const hostname = '127.0.0.1';
 
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.xml({ type: 'text/xml' }));
+const morganFormat = ':method \t :url \t :status \t  :response-time ms';
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs.log'), {
+  flags: 'a'
+});
+
+app.use(morgan(morganFormat, { stream: accessLogStream }));
 
 const getData = (data) => covid19ImpactEstimator(data);
 
 app.get('/', (req, res) => {
-  res.send('Express is working well');
+  res.send('Covid-19 estimator api');
 });
 
 app.post('/api/v1/on-covid-19', (req, res) => {
@@ -31,7 +40,13 @@ app.post('/api/v1/on-covid-19/json', (req, res) => {
   res.json(getData(req.body));
 });
 
-app.post('/api/v1/on-covid-19/logs', (req, res) => {});
+app.post('/api/v1/on-covid-19/logs', (req, res) => {
+  const readStream = fs.createReadStream(path.join(__dirname, 'logs.log'));
+  readStream.on('open', () => {
+    readStream.pipe(res);
+  });
+  res.setHeader('Content-Type', 'plain/text');
+});
 
 app.listen(port, () => {
   console.log(`Running on http://${hostname}:${port}`);
